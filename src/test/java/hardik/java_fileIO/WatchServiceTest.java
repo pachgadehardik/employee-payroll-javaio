1a4c92d1c587f9a0d5cd856fd23f5d9b8e1d3d43
pachgadehardik <pachgadehardik@gmail.com>
2020-10-13 19:10:27 +0530

    Added WatchService Functionality using TDD

--- src/test/java/hardik/java_fileIO/WatchServiceTest.java
+++ src/test/java/hardik/java_fileIO/WatchServiceTest.java
@@ -10,20 +10,12 @@ import java.util.stream.IntStream;
 
 import org.junit.Test;
 
-import com.capg.javaio.DeleteFiles;
 import com.capg.javaio.JavaWatchService;
 
-public class NIOFileTest {
+public class WatchServiceTest {
 
 	private static String HOME = "H:\\Capgemini\\Capg_Training";
 	private static String PLAY_WITH_NIO = "TempPlay";
-
-	@Test
-	public void givenPathWhenCheckedThenConfirm() throws IOException {
-		// Check Whether File/Directory Exists
-		Path homePath = Paths.get(HOME);
-		assertTrue(Files.exists(homePath));
-	}
 
 	@Test
 	public void createTempDirectory() throws IOException {
@@ -46,21 +38,12 @@ public class NIOFileTest {
 			assertTrue(Files.exists(tempFile));
 
 		});
-
-		// Listing files
-		Files.newDirectoryStream(playPath).forEach(System.out::println);
-		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
-				.forEach(System.out::println);
 	}
-
+	
 	@Test
-	public void deleteFilesIfExisting() throws IOException {
-		// Delete Files/Directory if Exists
-		Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
-
-		boolean result = DeleteFiles.deleteDirectory(playPath.toFile());
-		assert (result);
-
+	public void givenADirectoryWhenWatchedListsAllTheActivities() throws IOException {
+		Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+		new JavaWatchService(dir).processEvents();
 	}
-
 }
